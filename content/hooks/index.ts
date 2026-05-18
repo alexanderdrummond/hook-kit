cc512c670e0c6330f2dd20d573d3ab76814acdeb
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2026-05-18 22:21:06 +0200

    feat: add multiple hooks

--- content/hooks/index.ts
+++ content/hooks/index.ts
@@ -1,4 +1,14 @@
 import * as useCopyToClipboard from "./use-copy-to-clipboard.mdx";
+import * as useDebounce from "./use-debounce.mdx";
+import * as useFormDirty from "./use-form-dirty.mdx";
+import * as useInputMask from "./use-input-mask.mdx";
+import * as useMediaQuery from "./use-media-query.mdx";
+import * as useOnClickOutside from "./use-on-click-outside.mdx";
+import * as useQueue from "./use-queue.mdx";
+import * as useScrollDirection from "./use-scroll-direction.mdx";
+import * as useScrollLock from "./use-scroll-lock.mdx";
+import * as useToggle from "./use-toggle.mdx";
+import * as useUndoRedo from "./use-undo-redo.mdx";
 import type { ComponentType } from "react";
 
 export type HookMetadata = {
@@ -16,6 +26,16 @@ type HookModule = {
 
 const hookModules = {
   "use-copy-to-clipboard": useCopyToClipboard as HookModule,
+  "use-debounce": useDebounce as HookModule,
+  "use-form-dirty": useFormDirty as HookModule,
+  "use-input-mask": useInputMask as HookModule,
+  "use-media-query": useMediaQuery as HookModule,
+  "use-on-click-outside": useOnClickOutside as HookModule,
+  "use-queue": useQueue as HookModule,
+  "use-scroll-direction": useScrollDirection as HookModule,
+  "use-scroll-lock": useScrollLock as HookModule,
+  "use-toggle": useToggle as HookModule,
+  "use-undo-redo": useUndoRedo as HookModule,
 } satisfies Record<string, HookModule>;
 
 export type HookSlug = keyof typeof hookModules;
